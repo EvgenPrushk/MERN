ee48880e001cf8a828079e5933e0f985ba87cac8
EvgenPrushk <ogmprushak@mail.ru>
2021-05-19 17:33:14 +0300

    feat: add input value in form

---
+++ goldman/src/context/alert/alertReducer.js
@@ -0,0 +1,12 @@
+import { HIDE_ALERT, SHOW_ALERT } from "../types";
+
+const hadlers = {
+  [SHOW_ALERT]: (state, { payload }) => ({ ...payload, visible: true }),
+  [HIDE_ALERT]: (state) => ({ ...state, visible: false }),
+  DEFAULT: (state) => state,
+};
+
+export const alertReducer = (state, action) => {
+  const handle = hadlers[action.type] || hadlers.DEFAULT;
+  return handle(state, action);
+};
